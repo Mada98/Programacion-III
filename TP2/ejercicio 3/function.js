cf867d0c357ae9f9265ba987f3e1fc71eb270566
Gabeiras <167128373+Mada98@users.noreply.github.com>
2025-04-28 15:17:11 -0300

    css correccion

--- TP2/ejercicio 3/function.js
+++ TP2/ejercicio 3/function.js
@@ -2,14 +2,14 @@ const botonResaltar = document.getElementById("botonRes")
 const botonOcultar = document.getElementById("botonOcu")
 const parrafos = document.querySelectorAll(".parrafo")
 
-botonResaltar.addEventListener("click", function(){
-    for(let i = 0; i < parrafos.length; i++){
+botonResaltar.addEventListener("click", function () {
+    for (let i = 0; i < parrafos.length; i++) {
         parrafos[i].classList.add('resaltado')
     }
 })
 
-botonOcultar.addEventListener("click", function(){
-    for(let i = 0; i < parrafos.length; i++){
+botonOcultar.addEventListener("click", function () {
+    for (let i = 0; i < parrafos.length; i++) {
         parrafos[i].classList.toggle('oculto')
     }
 })
