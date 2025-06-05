e0822b3121a92bd5f530e1a867199ce3895a9e9b
MatAviez05 <baezavilamateo@gmail.com>
2025-06-05 20:26:09 -0300

    post turnos.web

--- TP3/src/routes/turnos.web.routes.js
+++ TP3/src/routes/turnos.web.routes.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 const turnosController = require('../controllers/API/turnos.controller')
+const { validate } = require('../middlewares/validate')
+const { turnoSchema } = require('../schemas/turnos.schema')
 const router = Router();
 
 router.get('/', turnosController.listaDeTurnosRender);
+router.post('/post', validate(turnoSchema.create), turnosController.createTurno);
 
 module.exports = router;
