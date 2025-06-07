a88f9f640185f8004c324733cfd1f9352f076e40
MatAviez05 <baezavilamateo@gmail.com>
2025-06-06 21:03:35 -0300

    readme update

--- TP3/src/routes/pacientes.web.routes.js
+++ TP3/src/routes/pacientes.web.routes.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 const pacientesController = require('../controllers/API/pacientes.controller')
+const { validate } = require('../middlewares/validate')
+const { pacienteSchema } = require('../schemas/paciente.schema')
 const router = Router();
 
 router.get('/', pacientesController.listPacientesRender);
+router.post('/post', validate(pacienteSchema.create), pacientesController.createPaciente)
 
 module.exports = router;
